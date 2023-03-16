19c84d56ae1c32d6432cec6cc1b652abae251c27
RelebohileTakalo <reletakalo@gmail.com>
2023-03-16 14:57:23 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,38 +2,22 @@
 #include <stdlib.h>
 
 /**
- * _realloc - Main Entry
- * @ptr: input
- * @old_size: input
- * @new_size: input
+ * _calloc - Main Entry
+ * @nmemb: input
+ * @size: input
  * Return: 0
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *p;
-unsigned int i, max = new_size;
-char *oldp = ptr;
+unsigned int i;
 
-if (ptr == NULL8)
-{
-p = malloc(new_size);
-return (p);
-}
-else if (new_size == 0)
-{
-free(ptr);
+if (nmemb == 0 || size == 0)
 return (NULL);
-}
-else if (new_size == old_size)
-return (ptr);
-
-p = malloc(new_size);
+p = malloc(nmemb * size);
 if (p == NULL)
 return (NULL);
-if (new_size > old_size)
-max = old_size;
-for (i = 0; i < max; i++)
-p[i] = oldp[i];
-free(ptr);
+for (i = 0; i < (nmemb * size); i++)
+p[i] = 0;
 return (p);
 }
