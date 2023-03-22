f10e27053f0526024299ec814315019ef5480888
RelebohileTakalo <reletakalo@gmail.com>
2023-03-22 14:03:57 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,83 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * len - find length of string
- * @str: string
- * Return: length
+ * new_dog - Entry point
+ * Description: Create new dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * * Return: A pointer to the new dog struct, or NULL if failed
  */
-int len(char *str)
-{
-int i;
 
-for (i = 0; *(str + i); i++)
-;
-return (i);
-}
-/**
- * strcpy - copies the string pointed to by src,
- * including the terminating null byte (\0),
- * to the buffer pointed to by dest
- * @dest: copy source to this buffer
- * @src: this is the source to copy
- * Return: copy of original source
- */
-char *strcpy(char *dest, char *src)
-{
-int i;
-
-for (i = 0; i <= len(src); i++)
-dest[i] = src[i];
-return (dest);
-}
-/**
- * new_dog - create new instance of struct dog
- * @name: member
- * @age: member
- * @owner: member
- * Return: initialized instance of struct dog
- */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *dog1;
-char *copy_of_name;
-char *copy_of_owner;
-
-dog1 = malloc(sizeof(dog_t)); /* validate if dog1 initiated correctly */
-if (dog1 == NULL)
+/* Allocate memory for the new dog struct */
+dog_t *max = malloc(sizeof(dog_t));
+/* Max just a dog's name */
+/* Check if memory allocation failed */
+if (max == NULL)
+{
 return (NULL);
-
-dog1->age = age;
-
-/* make copies of struct members and validate, else free on error */
-/* set values of struct members to copies of arguments or set to NULL */
-if (name != NULL)
+		}
+/* Allocate memory for the name string */
+max->name = malloc(sizeof(char) * (strlen(name) + 1));
+if (max->name == NULL)
 {
-copy_of_name = malloc(len(name) + 1);
-if (copy_of_name == NULL)
-{
-free(dog1);
+free(max);
 return (NULL);
 }
-dog1->name = strcpy(copy_of_name, name);
-}
-else
-dog1->name = NULL;
-
-if (owner != NULL)
+/* Allocate memory for the owner string */
+max->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+if (max->owner == NULL)
 {
-copy_of_owner = malloc(len(owner) + 1);
-if (copy_of_owner == NULL)
-{
-free(copy_of_name);
-free(dog1);
+free(max->name);
+free(max);
 return (NULL);
 }
-dog1->owner = strcpy(copy_of_owner, owner);
-}
-else
-dog1->owner = NULL;
 
-return (dog1);
+/* Copy the name and owner strings to the new struct */
+strcpy(max->name, name);
+strcpy(max->owner, owner);
+/* Set the age of the new dog */
+max->age = age;
+
+return (max);
 }
+
